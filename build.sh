26511e157f020125beb69fccc7c12b16e190b965
Huỳnh Đình Quí <deathbook1999@gmail.com>
2024-06-23 14:51:15 +0700

    build: feeds.conf renamed

--- build.sh
+++ build.sh
@@ -12,13 +12,16 @@ sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bis
 export OP_BUILD_PATH=$PWD
 git clone https://github.com/coolsnowwolf/lede.git
 cd "${OP_BUILD_PATH}"/lede || exit
+# start: git config
+
+# end: git config
 # start: revert commit upx: bump to v4.2.2 [https://github.com/coolsnowwolf/lede/commit/24e2cfc64fb1072145b2a57a3702fec204e900ad]
 git revert 24e2cfc64fb1072145b2a57a3702fec204e900ad
 # end: revert commit upx: bump to v4.2.2 [https://github.com/coolsnowwolf/lede/commit/24e2cfc64fb1072145b2a57a3702fec204e900ad]
 ./scripts/feeds update -a && ./scripts/feeds install -a
-rm -rf ./tmp && rm -rf .config && rm -rf .feeds.conf
+rm -rf ./tmp && rm -rf .config && rm -rf feeds.conf
 mv "${OP_BUILD_PATH}"/.config "${OP_BUILD_PATH}"/lede/.config
-mv "${OP_BUILD_PATH}"/.feeds.conf "${OP_BUILD_PATH}"/lede/.feeds.conf
+mv "${OP_BUILD_PATH}"/feeds.conf "${OP_BUILD_PATH}"/lede/feeds.conf
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 make defconfig
 make download -j8f
