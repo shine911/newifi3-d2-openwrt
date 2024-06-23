969f810da74fa9a967c30fffeae76ccf97e9036e
Huỳnh Đình Quí <deathbook1999@gmail.com>
2024-06-22 23:36:48 -0500

    build: add libxtables-dev

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bis
   libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz \
   mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pyelftools \
   libpython3-dev qemu-utils rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip \
-  vim wget xmlto xxd zlib1g-dev
+  vim wget xmlto xxd zlib1g-dev libxtables-dev
 
 export OP_BUILD_PATH=$PWD
 git clone https://github.com/coolsnowwolf/lede.git
-
+# Patch
+rm -rf 
+git clone https://github.com/coolsnowwolf/routing/tree/5437d2c91fd9f15e06fbea46677abb529ed3547c 
 cd "${OP_BUILD_PATH}"/lede || exit
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
